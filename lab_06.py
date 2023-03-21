0276a668d3ca5f050a8e5a48ed25d6fb86e5f706
Jia Johnson <jiajohnson@MacBook-Pro-202.local>
2023-03-20 22:46:27 -0400

    decoder function and changes to option 2 to decode encoded password.

--- lab_06.py
+++ lab_06.py
@@ -9,20 +9,18 @@ def encode(string):
         encoded_list += list[idx]
     return encoded_list
 
-'''def decode(string):
+def decode(string): #decoder function
     list = []
     for i in string:
         list.append(int(i))
-    encoded_list = ''
+    encoded_list = ' '
     for idx, j in enumerate(list):
         list[idx] = str(j - 3)
         encoded_list += list[idx]
-    return encoded_list'''
-
+    return encoded_list
 
 def menu():
     return print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
-
 
 if __name__ == '__main__':
     while True:
@@ -32,7 +30,8 @@ if __name__ == '__main__':
             string = input('Please enter your password to encode: ')
             encoded_password = encode(string)
             print('Your password has been encoded and stored!')
-        elif option == '2':
+        elif option == '2': # prints the encoded password and the decoded password (original)
+            string = decode(encoded_password)
             print(f'The encoded password is {encoded_password}, and the original password is {string}.')
         elif option == '3':
             break
